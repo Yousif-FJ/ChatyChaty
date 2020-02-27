2ea3547c32d4be1de1c7cf3fe3bf4ad0d9d4200f
yousef_fj <yousef.fawzy.2000@gmail.com>
2020-02-28 00:55:26 +0300

    Add chatychaty context and base repository

---
+++ Model/MessagesRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatyChaty.Model
+{
+    public class MessagesRepository :IMessageRepository
+    {
+        private readonly ChatyChatyContext chatyChatyContext;
+
+        public MessagesRepository(ChatyChatyContext chatyChatyContext) 
+        {
+            this.chatyChatyContext = chatyChatyContext;
+        }
+
+        public IEnumerable<Message> GetAllMessages()
+        {
+            return chatyChatyContext.MessagesSet;
+        }
+    }
+}
