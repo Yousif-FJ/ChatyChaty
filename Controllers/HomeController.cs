60fc6d221f4758914b137ffd64f107864a06657b
yousef_fj <yousef.fawzy.2000@gmail.com>
2020-02-27 13:57:49 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ChatyChaty.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment webHost;
+
+        public HomeController(ILogger<HomeController> logger , IWebHostEnvironment webHost )
+        {
+            _logger = logger;
+            this.webHost = webHost;
+        }
+
+        [HttpGet]
+        [Route("Index")]
+        public string Index()
+        {
+           _logger.LogInformation(Environment.GetEnvironmentVariable("DATABASE_URL"));
+            return "App running";
+        }
+
+
+        [HttpGet]
+        [Route("GetImg")]
+        public string GetImg()
+        {
+            return Path.Combine(webHost.WebRootPath, "img", "CatFilterReaction.jpg");
+        }
+    }
+}
---
+++ Model/ChatyChatyContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatyChaty.Model
+{
+    public class ChatyChatyContext:DbContext
+    {
+        public DbSet<Message> MessagesSet { get; set; }
+    }
+}
+
