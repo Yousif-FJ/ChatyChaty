d861230f37e0d4b119738828243b43a56e71869b
yousef_fj <yousef.fawzy.2000@gmail.com>
2020-03-06 19:12:53 +0300

    Add Account model and authentication result model

---
+++ Model/AccountModels/AuthenticationResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatyChaty.Model.AccountModels
+{
+    public class AuthenticationResult
+    {
+        public bool Success { get; set; }
+        public string Token { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
