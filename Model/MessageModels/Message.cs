98abe46fdb3cece65e553afd0613d84a8bb6e7f2
yousef_fj <yousef.fawzy.2000@gmail.com>
2020-03-06 19:13:51 +0300

    Change in messages and model and file structure

--- Model/MessageModels/Message.cs
+++ Model/MessageModels/Message.cs
@@ -4,10 +4,11 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ChatyChaty.Model
+namespace ChatyChaty.Model.MessageModels
 {
     public class Message
     {
+        [Key]
         public long ID { get; set; }
         [Required]
         public string Sender { get; set; }
--- Model/MessageModels/MessagesRepository.cs
+++ Model/MessageModels/MessagesRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ChatyChaty.Model
+namespace ChatyChaty.Model.MessageModels
 {
     public class MessagesRepository :IMessageRepository
     {
