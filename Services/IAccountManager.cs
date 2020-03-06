fc9ff3db91298d55bc5afb5f3fe83e7369f7a73e
yousef_fj <yousef.fawzy.2000@gmail.com>
2020-03-06 19:58:26 +0300

    Add account manager

---
+++ Services/IAccountManager.cs
@@ -0,0 +1,14 @@
+﻿using ChatyChaty.Model.AccountModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatyChaty.Services
+{
+    public interface IAccountManager
+    {
+        Task<AuthenticationResult> CreateAccount(AccountModel accountModel);
+        Task<AuthenticationResult> Login(AccountModel accountModel);
+    }
+}
