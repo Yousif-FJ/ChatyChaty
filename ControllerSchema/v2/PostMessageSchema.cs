a8fe562c070abf474beada31de126c2df3821c88
yousef_fj <yousef.fawzy.2000@gmail.com>
2020-03-09 01:32:00 +0300

    Implment GetNewMessages method and changes to improve versioning and preformace

--- ControllerSchema/v2/PostMessageSchema.cs
+++ ControllerSchema/v2/PostMessageSchema.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace ChatyChaty.ControllerSchema.v2
 {
     public class PostMessageSchema
     {
+        [Required]
         public string Body { get; set; }
     }
 }
