d31d84aaca987106f9ec0e048e26b3d2b924cd50
yousef_fj <yousef.fawzy.2000@gmail.com>
2020-03-11 23:03:19 +0300

    Add file repository for saving pictures

---
+++ Model/FileRepository/FileRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatyChaty.Model.FileRepository
+{
+    public class FileRepository : IFileRepository
+    {
+        private readonly IWebHostEnvironment hostEnvironment;
+
+        public FileRepository(IWebHostEnvironment hostEnvironment)
+        {
+            this.hostEnvironment = hostEnvironment;
+        }
+
+        public async Task<string> ChangePhoto(string PhotoName, IFormFile formFile)
+        {
+            var FilePath = Path.Combine(hostEnvironment.WebRootPath, "ProfilePIctures", PhotoName);
+            using FileStream fileStream = new FileStream(path: FilePath, FileMode.Create);
+            await formFile.CopyToAsync(fileStream);
+            return PhotoName;
+        }
+
+        public async Task<string> UploadPhoto(IFormFile formFile)
+        {
+            var PhotoName = $"{Guid.NewGuid().ToString()}_{formFile.FileName}";
+            var FilePath = Path.Combine(hostEnvironment.WebRootPath, "ProfilePIctures", PhotoName);
+            using FileStream fileStream = new FileStream(path: FilePath, FileMode.CreateNew);
+            await formFile.CopyToAsync(fileStream);
+            return PhotoName;
+        }
+    }
+}
---
+++ Model/FileRepository/IFileRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatyChaty.Model.FileRepository
+{
+    public interface IFileRepository
+    {
+        public Task<string> UploadPhoto(IFormFile formFile);
+        public Task<string> ChangePhoto(string PhotoName, IFormFile formFile);
+    }
+}
