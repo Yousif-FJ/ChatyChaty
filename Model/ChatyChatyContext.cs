87d3465bf348e28b2782b08ec8723e8560af4db7
yousef_fj <yousef.fawzy.2000@gmail.com>
2020-03-14 00:16:28 +0300

    Chage DB context to not include connection string

--- Model/ChatyChatyContext.cs
+++ Model/ChatyChatyContext.cs
@@ -31,8 +31,7 @@ namespace ChatyChaty.Model
                 }
                 else
                 {
-                    logger.LogWarning("DATABASE_URL is null, falling back to inline Database connection");
-                    databaseUrl = "postgres://ariehsrkswnkuy:f71fcbb30ddc875d91836b4ca0c0ca4af1da51b3885d73674381e0888ac757d5@ec2-54-75-231-215.eu-west-1.compute.amazonaws.com:5432/dkmm2e5rvrrn1";
+                throw new Exception("Couldn't get connection string");
                 }
                 var databaseUri = new Uri(databaseUrl);
                 var userInfo = databaseUri.UserInfo.Split(':');
