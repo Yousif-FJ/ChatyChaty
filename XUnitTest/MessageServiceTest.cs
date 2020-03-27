0197a4118e766c3e90bcdc4948a56cf9fc6af1ed
yousef_fj <yousef.fawzy.2000@gmail.com>
2020-03-27 12:09:14 +0300

    Add more unit tests for MessageService

--- XUnitTest/MessageServiceTest.cs
+++ XUnitTest/MessageServiceTest.cs
@@ -26,20 +26,11 @@ namespace XUnitTest
 
 
         [Fact]
-        public async Task GetConversation()
+        public async Task NewConversation_success()
         {
             //Arrange
-            var sender = new AppUser("Test1") 
-            { 
-                Id = 1
-            };
-
-            var reciver = new AppUser("Test2")
-            {
-                Id = 2
-            };
-
-            await dbContext.Users.AddRangeAsync(sender,reciver);
+            var sender = (await dbContext.Users.AddAsync(new AppUser("Test1"))).Entity;
+            var reciver = (await dbContext.Users.AddAsync(new AppUser("Test2"))).Entity;
             dbContext.SaveChanges();
             //Act
             var ConversationId = await messageService.NewConversation(sender.Id, reciver.Id);
@@ -51,36 +42,114 @@ namespace XUnitTest
         }
 
         [Fact]
-        public async Task SendMessage_Test()
+        public async Task SendMessage_success()
         {
             //Arrange
-            var sender = new AppUser("Test1")
-            {
-                Id = 1
-            };
-
-            var reciver = new AppUser("Test2")
-            {
-                Id = 2
-            };
-
-            var conversation = await dbContext.Conversations.AddAsync(new Conversation
+            var sender = (await dbContext.Users.AddAsync(new AppUser("Test1"))).Entity;
+            var reciver = (await dbContext.Users.AddAsync(new AppUser("Test2"))).Entity;
+            var conversation = (await dbContext.Conversations.AddAsync(new Conversation
             {
                 FirstUserId = sender.Id,
                 SecondUserId = reciver.Id
-            });
+            })).Entity;
 
-
-            await dbContext.Users.AddRangeAsync(sender, reciver);
-            dbContext.SaveChanges();
+            await dbContext.SaveChangesAsync();
 
             string message = "Some test message";
             //Act
-            await messageService.SendMessage(conversation.Entity.Id, sender.Id, message);
+            await messageService.SendMessage(conversation.Id, sender.Id, message);
             //Assert
             var m = await dbContext.Messages.LastAsync();
             Assert.True(m.Body == message);
         }
-    }
 
+        [Fact]
+        public async Task GetNewMessages_ZeroAsMessageId()
+        {
+            //Arrange
+            var u1 = (await dbContext.Users.AddAsync(new AppUser("Test1"))).Entity;
+            var u2 = (await dbContext.Users.AddAsync(new AppUser("Test2"))).Entity;
+            var conversation1 = (await dbContext.Conversations.AddAsync(new Conversation
+            {
+                FirstUserId = u1.Id,
+                SecondUserId = u2.Id
+            })).Entity;
+
+            var Messages = new List<Message>
+            {
+                new Message
+                {
+                    Body = "test1",
+                    ConversationId = conversation1.Id,
+                    SenderId = u1.Id
+                },
+                new Message
+                {
+                    Body = "test2",
+                    ConversationId = conversation1.Id,
+                    SenderId = u2.Id
+                }
+            };
+
+            await dbContext.AddRangeAsync(Messages);
+            await dbContext.SaveChangesAsync();
+
+
+            //Act
+            var MessagesResult = await messageService.GetNewMessages(u1.Id, 0);
+            //Assert
+            var Messagelist = new List<Message>(MessagesResult);
+            Assert.True(Messagelist[0].Body == "test1");
+            Assert.True(Messagelist[1].Body == "test2");
+            Assert.True(Messagelist[0].ConversationId == conversation1.Id);
+        }
+
+        [Fact]
+        public async Task GetNewMessages_CorrectChat()
+        {
+            //Arrange
+            var u1 = (await dbContext.Users.AddAsync(new AppUser("Test1"))).Entity;
+            var u2 = (await dbContext.Users.AddAsync(new AppUser("Test2"))).Entity;
+            var u3 = (await dbContext.Users.AddAsync(new AppUser("Test3"))).Entity;
+            var conversation1 = (await dbContext.Conversations.AddAsync(new Conversation
+            {
+                FirstUserId = u1.Id,
+                SecondUserId = u2.Id
+            })).Entity;
+
+            var conversation2 = (await dbContext.Conversations.AddAsync(new Conversation
+            {
+                FirstUserId = u1.Id,
+                SecondUserId = u3.Id
+            })).Entity;
+
+            var Messages = new List<Message>
+            {
+                new Message
+                {
+                    Body = "test1",
+                    ConversationId = conversation1.Id,
+                    SenderId = u1.Id
+                },
+                new Message
+                {
+                    Body = "test3",
+                    ConversationId = conversation2.Id,
+                    SenderId = u1.Id
+                }
+            };
+
+            await dbContext.AddRangeAsync(Messages);
+            await dbContext.SaveChangesAsync();
+
+
+            //Act
+            var MessagesResult = await messageService.GetNewMessages(u2.Id, 0);
+            //Assert
+            var Messagelist = new List<Message>(MessagesResult);
+            Assert.True(Messagelist[0].Body == "test1");
+            Assert.True(Messagelist[0].ConversationId == conversation1.Id);
+            Assert.True(Messagelist.Count == 1);
+        }
+    }
 }
