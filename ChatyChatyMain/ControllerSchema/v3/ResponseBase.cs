0e561a4dd6b3518189cb5c06e50567aa85d1f693
yousef_fj <yousef.fawzy.2000@gmail.com>
2020-06-01 19:27:09 +0300

    Add ResponseBase and CustomValidationAttribute
    Add new ReponseBase class and Error class to be the base the of all requests, and add CustomModelValidation Filter Attribute to make cutsom model error response

---
+++ ChatyChatyMain/ControllerSchema/v3/ResponseBase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatyChaty.ControllerSchema.v3
+{
+    public class ResponseBase
+    {
+        public bool Success { get; set; }
+        public IEnumerable<Error> Errors { get; set; }
+        public virtual object Data { get; set; }
+
+        public ResponseBase(){}
+
+        public ResponseBase(ModelStateDictionary modelState)
+        {
+            if (modelState.IsValid)
+            {
+                throw new InvalidOperationException("ModelState is valid, expected invalid modelstate");
+            }
+            Success = false;
+            Errors = modelState.Keys
+                .SelectMany(key => modelState[key].Errors.Select(x => new Error(key, x.ErrorMessage)))
+                .ToList();
+        }
+    }
+}
---
+++ ChatyChatyMain/ValidationAttribute/CustomModelValidationResponseAttribute.cs
@@ -0,0 +1,25 @@
+﻿using ChatyChaty.ControllerSchema.v3;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatyChaty.ValidationAttribute
+{
+    public class CustomModelValidationResponseAttribute : ActionFilterAttribute
+    {
+        public async override Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(new ResponseBase(context.ModelState));
+            }
+            else
+            {
+                await next();
+            }
+        }
+    }
+}
