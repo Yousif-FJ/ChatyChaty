171dad1699fc0f4b36df0028acb7338fec1f92ca
yousef_fj <yousef.fawzy.2000@gmail.com>
2020-06-29 20:14:34 +0300

    Add back jwt authentication from qurry, changes to hub client and more
    Add real time razer page compilation
    Client now generate html for each action given the method name and description, this should make adding methods to the client easier in the future

--- ChatyChatyMain/wwwroot/js/Script.js
+++ ChatyChatyMain/wwwroot/js/Script.js
@@ -1,4 +1,5 @@
 ﻿"use strict";
+//the method get called after the user input the token
 function Authenticate() {
     //close existing connection
     if (connection != null) {
@@ -17,24 +18,55 @@ function Authenticate() {
     });
 
     //list of response methods to listen to 
-    let MethodList = ["TestResponse"];
+    const MethodResponseList = ["TestResponse"];
 
     //connect and listen to the methods
-    for (let i = 0; i < MethodList.length; i++) {
+    for (let i = 0; i < MethodResponseList.length; i++) {
 
-    connection.on(MethodList[i], function (message) {
-        var li = document.createElement("li");
-        li.textContent = "Response from "+ MethodList[i]+" : "+message;
-        document.getElementById("messagesList").appendChild(li);
-    });
-    }
-
-    //call method
-    document.getElementById("sendButton").addEventListener("click", function (event) {
-        var message = document.getElementById("messageInput").value;
-        connection.invoke("SendTest", message).catch(function (err) {
-            return console.error(err.toString());
+        connection.on(MethodResponseList[i], function (message) {
+            var li = document.createElement("li");
+            li.textContent = "Response from " + MethodResponseList[i] + " : " + message;
+            document.getElementById("messagesList").appendChild(li);
         });
-        event.preventDefault();
-    });
+    }
+    //list of methods
+    const MethodList = ["SendTest"];
+    //list of method description
+    const MethodDesciptionList = ["Send a test message which the server will echo back to caller"];
+
+    //generate html for the list of methods 
+    //get tamplate html
+    const tamplateForm = document.getElementById("tamplateForm");
+    const tamplateLi = document.getElementById("tamplateLi");
+
+    for (let i = 0; i < MethodList.length; i++) {
+        //processing the form
+        //clone existing html to reuse it 
+        let newForm = tamplateForm.cloneNode(true);
+        newForm.id = MethodList[i] + "Form";
+        newForm.removeAttribute("hidden");
+        let lable = newForm.childNodes[1];
+        lable.innerHTML = MethodDesciptionList[i];
+        let input = newForm.childNodes[3];
+        input.id = MethodList[i] + "Input";
+        let button = newForm.childNodes[5];
+
+        //add event listener to the button
+        button.addEventListener("click", function (event) {
+            var message = input.value;
+            connection.invoke(MethodList[i], message).catch(function (err) {
+                return console.error(err.toString());
+            });
+            event.preventDefault();
+        });
+        document.getElementById("myTabContent").appendChild(newForm);
+
+        //processing the list item
+        let newLi = tamplateLi.cloneNode(true);
+        newLi.removeAttribute("hidden");
+        newLi.id = MethodList[i] + "Li";
+        newLi.childNodes[1].setAttribute("href", "#" + newForm.id);
+        newLi.childNodes[1].innerHTML = MethodList[i];
+        document.getElementById("myTab").appendChild(newLi);
+    }
 }
