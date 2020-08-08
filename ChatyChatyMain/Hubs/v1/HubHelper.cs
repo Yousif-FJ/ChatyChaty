e46262518a18aef18f8b40e76c755be2fbc752c8
yousef_fj <yousef.fawzy.2000@gmail.com>
2020-08-09 01:27:34 +0300

    Add HubHelper
    A class used to preform SignalR actions outside MainHub, Implment sendUpdate to push message updates to clients.
    Update IChatyClient.

---
+++ ChatyChatyMain/Hubs/v1/HubHelper.cs
@@ -0,0 +1,70 @@
+﻿using ChatyChaty.ControllerSchema.v3;
+using ChatyChaty.Services;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ChatyChaty.Hubs.v1
+{
+    /// <summary>
+    /// This class is used when the hub is called outside of the MainHub classes
+    /// </summary>
+    public class HubHelper
+    {
+        private readonly IHubContext<MainHub, IChatClient> hubContext;
+        private readonly IMessageService messageService;
+        private readonly HubClientsStateManager stateManager;
+
+        public HubHelper(IHubContext<MainHub, IChatClient> hubContext,
+            IMessageService messageService,
+            HubClientsStateManager stateManager
+            )
+        {
+            this.hubContext = hubContext;
+            this.messageService = messageService;
+            this.stateManager = stateManager;
+        }
+        /// <summary>
+        /// Method called to send message updates to a connected client, return false if the client is not connected
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public async Task<bool> SendUpdate(long userId)
+        {
+            var clientState = stateManager.GetClient(userId);
+            if (clientState == null)
+            {
+                return false;
+            }
+
+            var result = await messageService.GetNewMessages(userId, clientState.LastMessageId);
+            string response;
+
+
+            var Messages = new List<MessageInfoBase>();
+            foreach (var message in result.Messages)
+            {
+                Messages.Add(new MessageInfoBase
+                {
+                    Body = message.Body,
+                    ChatId = message.ConversationId,
+                    MessageId = message.Id,
+                    Sender = message.Sender.UserName,
+                    Delivered = message.SenderId == userId ? message.Delivered : (bool?)null
+                }); ;
+            }
+            response = new ResponseBase<IEnumerable<MessageInfoBase>>
+            {
+                Success = true,
+                Data = Messages
+            }.ToJson();
+
+            _ = hubContext.Clients.User(userId.ToString()).UpdateMessagesResponses(response);
+            return true;
+        }
+    }
+}
--- ChatyChatyMain/Hubs/v1/IChatyClient.cs
+++ ChatyChatyMain/Hubs/v1/IChatyClient.cs
@@ -5,8 +5,13 @@ using System.Threading.Tasks;
 
 namespace ChatyChaty.Hubs.v1
 {
+    /// <summary>
+    /// An interface for SignalR hub, that represent responses called by the server to the clients
+    /// </summary>
     public interface IChatClient
     {
         Task TestResponse(string message);
+        Task InvalidJsonResponse(string meessga);
+        Task UpdateMessagesResponses(string JsonMessagesList);
     }
 }
