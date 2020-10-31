7c5a419dc93db6f8056f57818a3301bd012ea47f
yousef_fj <yousef.fawzy.2000@gmail.com>
2020-10-31 09:04:42 +0300

    Add Basic App entities

---
+++ ChatyChatyClient/Entities/Chat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatyChatyClient.Entities
+{
+    public class Chat
+    {
+        public long Id { get; set; }
+        public string ImgLink { get; set; }
+        public string ReceiverName { get; set; }
+        public IList<Message> Messages { get; set; }
+    }
+}
