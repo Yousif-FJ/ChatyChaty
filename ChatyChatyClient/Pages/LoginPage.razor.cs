68ab6ec275ecc85a94cfca87f1263731817d537b
yousef_fj <yousef.fawzy.2000@gmail.com>
2020-11-15 23:42:36 +0300

    Add login and signup page

---
+++ ChatyChatyClient/Pages/LoginPage.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatyChatyClient.Pages
+{
+    public partial class LoginPage
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public void Login()
+        {
+
+        }
+    }
+}
---
+++ ChatyChatyClient/Pages/SignUpPage.razor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatyChatyClient.Pages
+{
+    public partial class SignUpPage
+    {
+        public string DisplayName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public void SignUp()
+        {
+
+        }
+    }
+}
