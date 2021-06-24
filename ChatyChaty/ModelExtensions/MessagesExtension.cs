b498e0032f0257277f6bfa7dc452918cdcf4a006
yousef fawzy <yousef.fawzy.2000@gmail.com>
2021-06-25 01:14:05 +0300

    Fix problem where delivery time is not null when it should be

--- ChatyChaty/ModelExtensions/MessagesExtension.cs
+++ ChatyChaty/ModelExtensions/MessagesExtension.cs
@@ -21,7 +21,7 @@ namespace ChatyChaty.ModelExtensions
                                 message.SenderUsername,
                                 message.Body,
                                 message.SentTime,
-                                message.StatusUpdateTime,
+                                message.SenderId == userId ? message.StatusUpdateTime : null,
                                 message.SenderId == userId ? message.Delivered : null
                             ));
             }
